37d6ab561951b9a4c326192decc042d67e038acb
Dylan Attal <attal.dylan@gmail.com>
2022-05-11 19:31:31 -0400

    sets up database through sql alchemy

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
+from enum import unique
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Aabhishek331@localhost/lexus'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text)
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 
 @app.route('/')
@@ -22,5 +52,4 @@ def submit():
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host="0.0.0.0", port=5001)
