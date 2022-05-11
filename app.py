18ee19a3d9d6fe4912c02d3539426a9814627e99
Dylan Attal <attal.dylan@gmail.com>
2022-05-11 19:35:19 -0400

    Can add feedback data to the database

--- app.py
+++ app.py
@@ -47,8 +47,14 @@ def submit():
         comments = request.form['comments']
 
         if customer == '' or dealer == '':
-            return render_template('index.html', message='Please enter required fields')
-        return render_template('success.html')
+            return render_template('index.html', message='Please enter required fields.')
+
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback.')
 
 
 if __name__ == '__main__':
