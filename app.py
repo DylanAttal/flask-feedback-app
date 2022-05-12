65d3916c15521895aff11843e770f107d606aafe
Dylan Attal <attal.dylan@gmail.com>
2022-05-11 20:18:51 -0400

    I think sqlalchemy dialect name changed slightly for heroku

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{postgres_password}@localhost/lexus'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xbqihkdjzljszt:bae8e617470601b6e9a62db7cac9eef57ca834d936f08d8265c78537c69953aa@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d1r3f7lerdbrau'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xbqihkdjzljszt:bae8e617470601b6e9a62db7cac9eef57ca834d936f08d8265c78537c69953aa@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d1r3f7lerdbrau'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
